c25a8fc23a54ffad5b5942195932298a8da86459
reeeeemo <robbie@oxley.ca>
2024-06-21 22:00:52 -0400

    [FIX] Files

--- OLLM/OLLM.py
+++ OLLM/OLLM.py
@@ -27,12 +27,8 @@ batch_size = 32
 
 
 import torch
-print(torch.cuda.is_available())
-print(torch.cuda.current_device())
-print(torch.cuda.get_device_name(torch.cuda.current_device()))
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
 
 '''
     Prepares dataset for training the model.
